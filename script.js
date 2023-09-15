43ff851fc7037e4a3c94caeb117df8872295078d
akramjunaid <akramjunaid600@gmail.com>
2023-09-15 19:40:57 +0530

    modified

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const itemClear = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
 
 function addItem(e){
     e.preventDefault();
@@ -19,7 +21,11 @@ function addItem(e){
 
     const button = createButton('remove-item btn-link text-red');
     li.appendChild(button);
+
+    // Add li to DOM
     itemList.appendChild(li);
+    checkUI();
+
     itemInput.value = '';    
 }
 
@@ -36,6 +42,51 @@ function createIcon(classes){
     return icon;
 }
 
+function removeItem(e){
+    if( e.target.parentElement.classList.contains('remove-item')){
+        if(confirm('Are you sure?')){
+            e.target.parentElement.parentElement.remove();
+            checkUI();
+        }
+    }
+}
+function removeAllItems(e){
+    while(itemList.firstChild){
+        itemList.removeChild(itemList.firstChild);
+        checkUI();
+    }
+}
+function filterItems(e){
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+        if(itemName.indexOf(text) != -1){
+            item.style.display ='flex';
+        }else {
+            item.style.display ='none';
+        }
+    })
+    
+}
+
+function checkUI(){
+const items = itemList.querySelectorAll('li');
+
+    if(items.length === 0){
+        itemClear.style.display = 'none';
+        itemFilter.style.display = 'none';
+    } else {
+        itemClear.style.display = 'block';
+        itemFilter.style.display = 'block';
+    }
+}
+
 // Event Listeners
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+itemClear.addEventListener('click', removeAllItems);
+itemFilter.addEventListener('input', filterItems);
 
+checkUI();
